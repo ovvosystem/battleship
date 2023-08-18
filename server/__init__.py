97aef69a128e972b4df13e0df8ec5f03b103443f
Solaris <ovvo.system@gmail.com>
2023-08-18 06:56:12 -0300

    feat: create views.py

--- server/__init__.py
+++ server/__init__.py
@@ -2,10 +2,13 @@ import os
 
 from flask import Flask
 
+from views import views
+
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
+    app.register_blueprint(views, url_prefix="/")
 
     return app
 
