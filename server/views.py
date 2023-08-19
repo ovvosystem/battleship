7d5d2ac19f9f8a18e6aff08f4bdceadd9ca668e0
Solaris <ovvo.system@gmail.com>
2023-08-19 10:56:30 -0300

    feat: create index.html

--- server/views.py
+++ server/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "Home Page"
+    return render_template("index.html")
