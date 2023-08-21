2832d3a27477a70ac3304b300766118317c4d8d0
Solaris <ovvo.system@gmail.com>
2023-08-21 13:59:49 -0300

    feat: add secret key to app config

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,18 @@
-import os
+from os import environ as env
 
+from dotenv import find_dotenv, load_dotenv
 from flask import Flask
 
 from server.views import views
 
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = env.get("SECRET_KEY")
     app.register_blueprint(views, url_prefix="/")
 
     return app
