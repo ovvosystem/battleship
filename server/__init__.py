57f9aff0f875fc29f80a7b82880725b2632273e1
Solaris <ovvo.system@gmail.com>
2023-08-21 15:55:53 -0300

    style: update __init__.py comments

--- server/__init__.py
+++ server/__init__.py
@@ -5,12 +5,17 @@ from flask import Flask
 
 from server.views import views
 
+# Loads environment variables
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
 def create_app():
-    # create and configure the app
+    """Creates and configures the app
+    
+    Returns:
+        object (Flask): app
+    """
     app = Flask(__name__)
     app.config["SECRET_KEY"] = env.get("SECRET_KEY")
     app.register_blueprint(views, url_prefix="/")
