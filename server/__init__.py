9f0a4783f162297fd204b3390b77c4f432d23f5f
Solaris <ovvo.system@gmail.com>
2023-08-22 17:50:09 -0300

    feat: create login manager

--- server/__init__.py
+++ server/__init__.py
@@ -3,6 +3,7 @@ from os import path, environ as env
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # Load environment variables
 ENV_FILE = find_dotenv()
@@ -33,6 +34,14 @@ def create_app():
 
     create_database(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
 
 
