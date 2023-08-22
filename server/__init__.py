31430ce9390e1084287fd53ad5def27b18663cab
Solaris <ovvo.system@gmail.com>
2023-08-22 07:44:28 -0300

    feat: connect app to database

--- server/__init__.py
+++ server/__init__.py
@@ -2,13 +2,15 @@ from os import environ as env
 
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask
-
-from server.views import views
+from flask_sqlalchemy import SQLAlchemy
 
 # Loads environment variables
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
+
+db = SQLAlchemy()
+DB_NAME = env.get("DB_NAME")
 
 def create_app():
     """Creates and configures the app
@@ -18,6 +20,10 @@ def create_app():
     """
     app = Flask(__name__)
     app.config["SECRET_KEY"] = env.get("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from server.views import views
     app.register_blueprint(views, url_prefix="/")
 
     return app
