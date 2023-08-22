7d190490b6db8aba5c91128a7745998724cc479d
Solaris <ovvo.system@gmail.com>
2023-08-22 10:20:47 -0300

    feat: create blueprint for auth templates

--- server/__init__.py
+++ server/__init__.py
@@ -25,7 +25,9 @@ def create_app():
     db.init_app(app)
 
     from server.views import views
+    from server.auth import auth
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/auth/")
 
     from server.models import User
 
