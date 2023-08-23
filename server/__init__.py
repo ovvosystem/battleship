675d1e198dc8fef720df9e05f4c0ec7277854103
Solaris <ovvo.system@gmail.com>
2023-08-23 08:42:33 -0300

    feat: create api blueprint

--- server/__init__.py
+++ server/__init__.py
@@ -27,8 +27,10 @@ def create_app():
 
     from server.views import views
     from server.auth import auth
+    from server.api import api
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth/")
+    app.register_blueprint(api, url_prefix="/api/")
 
     from server.models import User
 
