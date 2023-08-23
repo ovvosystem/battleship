a479085f9b81c7527c996f423b3e3fbb082ada25
Solaris <ovvo.system@gmail.com>
2023-08-23 16:45:13 -0300

    feat: give feedback on non-existant room code

--- server/views.py
+++ server/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
+
+rooms = {}
 
 @views.route("/")
 def home():
@@ -13,5 +15,18 @@ def home():
     return render_template("index.html", user=current_user)
 
 @views.route("/play", methods=["GET", "POST"])
+@login_required
 def play():
+    if request.method == "POST":
+        create = request.form.get("create", False)
+        join = request.form.get("join", False)
+        code = request.form.get("code")
+        if code:
+            code = code.upper()
+
+        if join != False and not code:
+            flash("Please input a room code to join", category="error")
+        elif join != False and code not in rooms:
+            flash(f'No room of code "{code}"', category="error")
+
     return render_template("play.html", user=current_user)
