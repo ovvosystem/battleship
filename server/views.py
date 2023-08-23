c85f076e8395237704245ecf726224dfe0644fe5
Solaris <ovvo.system@gmail.com>
2023-08-23 17:11:50 -0300

    feat: generate game rooms

--- server/views.py
+++ server/views.py
@@ -1,9 +1,23 @@
+import random
+from string import ascii_uppercase
+
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+
+from server.apps.game import Game
 
 views = Blueprint("views", __name__)
 
 rooms = {}
+
+def generate_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            return code
 
 @views.route("/")
 def home():
@@ -28,5 +42,9 @@ def play():
             flash("Please input a room code to join", category="error")
         elif join != False and code not in rooms:
             flash(f'No room of code "{code}"', category="error")
+        elif create != False:
+            room = generate_code(6)
+            game = Game()
+            rooms[room] = {"players": 0, "game": game}
 
     return render_template("play.html", user=current_user)
