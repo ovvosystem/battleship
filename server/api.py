85a00acc4e0b8c9a13e9c1c10e0872ab780faaf1
Solaris <ovvo.system@gmail.com>
2023-08-24 09:49:53 -0300

    feat: create get_board function in API

--- server/api.py
+++ server/api.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, request, jsonify, session
 from flask_login import current_user
-from flask_socketio import join_room, leave_room
+from flask_socketio import join_room, leave_room, emit
 
 from . import socketio
 from server.views import rooms
 
 api = Blueprint("api", __name__)
+
+
+# Game events
+
+def get_boards(room):
+    if current_user == rooms[room]["creator"]:
+        game = rooms[room]["game"]
+        return {"player_board": game.player1_board.get_board(),
+                "opponent_board": game.player2_board.get_secret_board()}
+
 
 # SocketIO events
 
