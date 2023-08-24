e76e388f15deb1fbc0048f9614e8f52fa8401927
Solaris <ovvo.system@gmail.com>
2023-08-24 18:53:13 -0300

    feat: render boards on the page

--- server/static/game.js
+++ server/static/game.js
@@ -1,6 +1,36 @@
 socketio = io();
 
+const playerBoard = document.getElementById("player-board");
+const opponentBoard = document.getElementById("opponent-board");
+
 socketio.on("update", (boards) => {
-    console.log(boards.player_board);
-    console.log(boards.opponent_board);
+    playerBoard.innerHTML = "";
+    opponentBoard.innerHTML = "";
+
+    for (row of boards.player_board) {
+        for (tile of row) {
+            playerBoard.appendChild(renderTile(tile));
+        }
+    }
+
+    for (row of boards.opponent_board) {
+        for (tile of row) {
+            opponentBoard.appendChild(renderTile(tile));
+        }
+    }
 })
+
+function renderTile(tile) {
+    const tileDiv = document.createElement("div");
+    tileDiv.classList.add("grid-element", "flex", "flex-center");
+
+    if (tile === "S") {
+        tileDiv.classList.add("ship-tile");
+    } else if (tile === "O") {
+        tileDiv.classList.add("miss-tile");
+    } else if (tile === "X") {
+        tileDiv.classList.add("hit-tile");
+    }
+
+    return tileDiv;
+}
